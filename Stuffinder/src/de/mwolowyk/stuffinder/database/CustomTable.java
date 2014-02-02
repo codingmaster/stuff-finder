71c33923b8a7a94f41c7af3913648082f3c98420
Michael Wolowyk <volovikm2@gmail.com>
2014-02-02 19:41:56 +0100

    add stufffinder based on phonegap

--- Stuffinder/src/de/mwolowyk/stuffinder/database/CustomTable.java
+++
@@ -1,8 +0,0 @@
-package de.mwolowyk.stuffinder.database;
-
-public abstract class CustomTable {
-	public static String TABLE_NAME;
-	public static final String ID = "_id";
-	public static final String NAME = "name";
-	
-}
